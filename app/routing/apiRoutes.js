c2792ebcc03cc39c1887b20808d4a06c6a4ab113
dnl092 <drogalsky14@my.whitworth.edu>
2019-07-08 20:22:39 -0700

    form done and post request functional

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,11 +3,12 @@
 module.exports = function (app) {
 
     app.get("/api/friends", function (req, res) {
-        
+        //TODO: grab data froms frinds.js
     });
 
     app.post("/api/friends", function (req, res) {
-        
+        //TODO: put data in friends.jss
+        console.log(req.body);
     });
 
 }
